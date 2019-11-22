c9d16084e8b9f5259556e881fd8519f4148943ec
Saatvik <tikoosaatvik@gmail.com>
2019-11-22 11:13:16 -0800

    mainData file generated for stanford

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,13 @@ gulp.task('createDataFile', function () {
         jsonData = JSON.parse(data);
         var result = [];
         var names = ['Similar Values', 'Different Values without any NA', 'Different values with NA'];
-        var data_structure = {
-            name: 'Stanford',
-            children: []
-        };
-        result.push(data_structure);
+        
         for (var i = 0; i < 3; i++) {
             data_structure = {
                 name: names[i],
                 children: []
             };
-            result[0]['children'].push(data_structure);
+            result.push(data_structure);
         }
         var group_names = ['Followers Less than 100', 'Followers between 100 and 500',
             'Followers between 500 and 1000', 'Followers between 1000 and 10000', 'Followers greater than 10000'];
@@ -29,7 +25,7 @@ gulp.task('createDataFile', function () {
                     name: group_names[j],
                     children: []
                 };
-                result[0]['children'][i]['children'].push(data_structure)
+                result[i]['children'].push(data_structure)
             }
         }
         var connectivity_names = ['Lowely Connected Nodes', 'Highly Connected Nodes'];
@@ -40,7 +36,7 @@ gulp.task('createDataFile', function () {
                         name: connectivity_names[k],
                         children: []
                     };
-                    result[0]['children'][i]['children'][j]['children'].push(data_structure)
+                    result[i]['children'][j]['children'].push(data_structure)
                 }
             }
         }
@@ -48,97 +44,97 @@ gulp.task('createDataFile', function () {
             if(jsonData[i].mTurk == jsonData[i].stanford){
                 if(jsonData[i].followers < 100){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][0]['children'][0]['children'][0]['children'].push(jsonData[i])
+                        result[0]['children'][0]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][0]['children'][0]['children'][1]['children'].push(jsonData[i])
+                        result[0]['children'][0]['children'][1]['children'].push(jsonData[i])
                     }                    
                 } else if(100 <= jsonData[i].followers && jsonData[i].followers < 500){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][0]['children'][1]['children'][0]['children'].push(jsonData[i])
+                        result[0]['children'][1]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][0]['children'][1]['children'][1]['children'].push(jsonData[i])
+                        result[0]['children'][1]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(500 <= jsonData[i].followers && jsonData[i].followers < 1000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][0]['children'][2]['children'][0]['children'].push(jsonData[i])
+                        result[0]['children'][2]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][0]['children'][2]['children'][1]['children'].push(jsonData[i])
+                        result[0]['children'][2]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(1000 <= jsonData[i].followers && jsonData[i].followers < 10000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][0]['children'][3]['children'][0]['children'].push(jsonData[i])
+                        result[0]['children'][3]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][0]['children'][3]['children'][1]['children'].push(jsonData[i])
+                        result[0]['children'][3]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(10000 <= jsonData[i].followers){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][0]['children'][4]['children'][0]['children'].push(jsonData[i])
+                        result[0]['children'][4]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][0]['children'][4]['children'][1]['children'].push(jsonData[i])
+                        result[0]['children'][4]['children'][1]['children'].push(jsonData[i])
                     }
                 }
             } else if(jsonData[i].mTurk != jsonData[i].stanford && jsonData[i].stanford != 'N/A'){
                 if(jsonData[i].followers < 100){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][1]['children'][0]['children'][0]['children'].push(jsonData[i])
+                        result[1]['children'][0]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][1]['children'][0]['children'][1]['children'].push(jsonData[i])
+                        result[1]['children'][0]['children'][1]['children'].push(jsonData[i])
                     }                    
                 } else if(100 <= jsonData[i].followers && jsonData[i].followers < 500){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][1]['children'][1]['children'][0]['children'].push(jsonData[i])
+                        result[1]['children'][1]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][1]['children'][1]['children'][1]['children'].push(jsonData[i])
+                        result[1]['children'][1]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(500 <= jsonData[i].followers && jsonData[i].followers < 1000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][1]['children'][2]['children'][0]['children'].push(jsonData[i])
+                        result[1]['children'][2]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][1]['children'][2]['children'][1]['children'].push(jsonData[i])
+                        result[1]['children'][2]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(1000 <= jsonData[i].followers && jsonData[i].followers < 10000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][1]['children'][3]['children'][0]['children'].push(jsonData[i])
+                        result[1]['children'][3]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][1]['children'][3]['children'][1]['children'].push(jsonData[i])
+                        result[1]['children'][3]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(10000 <= jsonData[i].followers){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][1]['children'][4]['children'][0]['children'].push(jsonData[i])
+                        result[1]['children'][4]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][1]['children'][4]['children'][1]['children'].push(jsonData[i])
+                        result[1]['children'][4]['children'][1]['children'].push(jsonData[i])
                     }
                 }
             } else if(jsonData[i].mTurk != jsonData[i].stanford && jsonData[i].stanford == 'N/A'){
                 if(jsonData[i].followers < 100){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][2]['children'][0]['children'][0]['children'].push(jsonData[i])
+                        result[2]['children'][0]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][2]['children'][0]['children'][1]['children'].push(jsonData[i])
+                        result[2]['children'][0]['children'][1]['children'].push(jsonData[i])
                     }                    
                 } else if(100 <= jsonData[i].followers && jsonData[i].followers < 500){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][2]['children'][1]['children'][0]['children'].push(jsonData[i])
+                        result[2]['children'][1]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][2]['children'][1]['children'][1]['children'].push(jsonData[i])
+                        result[2]['children'][1]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(500 <= jsonData[i].followers && jsonData[i].followers < 1000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][2]['children'][2]['children'][0]['children'].push(jsonData[i])
+                        result[2]['children'][2]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][2]['children'][2]['children'][1]['children'].push(jsonData[i])
+                        result[2]['children'][2]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(1000 <= jsonData[i].followers && jsonData[i].followers < 10000){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][2]['children'][3]['children'][0]['children'].push(jsonData[i])
+                        result[2]['children'][3]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][2]['children'][3]['children'][1]['children'].push(jsonData[i])
+                        result[2]['children'][3]['children'][1]['children'].push(jsonData[i])
                     }
                 } else if(10000 <= jsonData[i].followers){
                     if(jsonData[i].totalTimesRetweeted / jsonData[i].followers <= 0.05){
-                        result[0]['children'][2]['children'][4]['children'][0]['children'].push(jsonData[i])
+                        result[2]['children'][4]['children'][0]['children'].push(jsonData[i])
                     } else {
-                        result[0]['children'][2]['children'][4]['children'][1]['children'].push(jsonData[i])
+                        result[2]['children'][4]['children'][1]['children'].push(jsonData[i])
                     }
                 }
             }
